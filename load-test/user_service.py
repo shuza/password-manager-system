47970c958423774c431c37325a21e7eb6a625552
Shuza <shuza.sa@gmail.com>
2019-09-03 20:02:50 +0600

    write load test script for login

---
+++ load-test/user_service.py
@@ -0,0 +1,15 @@
+from locust import HttpLocust, TaskSet, task
+
+class UserBehavior(TaskSet):
+    def login(self):
+        body = {
+            "email": "user-1@gmail.com"
+            "password": "123456"
+        }
+        self.client.post("/user/api/v1/auth/login", body)
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 500
+    max_wait = 5000
